c1b14e5721ee748a945e9aa80ca8b2da8a423e46
Jmlee <janinemllee@gmail.com>
2022-03-22 16:49:07 -0400

    changed user model and fixed movie service

--- src/main/java/com/favmovies/Main.java
+++ src/main/java/com/favmovies/Main.java
@@ -2,11 +2,12 @@ package com.favmovies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan({"com.genspark.springbootPractice.services","com.genspark.springbootPractice.repositories"})
+@ComponentScan(basePackages = "com.favmovies")
 public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
--- src/main/java/com/favmovies/controllers/MovieController.java
+++ src/main/java/com/favmovies/controllers/MovieController.java
@@ -3,16 +3,18 @@ package com.favmovies.controllers;
 import com.favmovies.models.Movie;
 import com.favmovies.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class MovieController {
 
     @Autowired
-    MovieService movieService;
+    private MovieService movieService;
 
     @GetMapping("/movies")
     public List<Movie> getUsersList(){
